5774215e9f78fc2d9f9479f82212ea446ccc4636
sayuen0 <sayuen0@gmail.com>
2023-10-01 19:51:33 +0900

    ストレートクラス一旦完成

--- src/lib/domain/model/hands/straight.test.ts
+++ src/lib/domain/model/hands/straight.test.ts
@@ -1,73 +1,244 @@
 import { PokerCard } from '@/lib/domain/model/card';
 import { Straight } from '@/lib/domain/model/hands/straight';
 
-describe('Straight.isHand', () => {
-  const testCases = [
-    {
-      name: '4枚以下の場合はストレートではない',
-      input: PokerCard.NewPokerCards('AS', '2D', '3H', '4C'),
-      expected: false,
-    },
-    {
-      name: 'A,2,3,4,5 should be straight',
-      input: PokerCard.NewPokerCards('AS', '2D', '3H', '4C', '5S'),
-      expected: true,
-    },
-    {
-      name: '2,3,4,5,6 should be straight',
-      input: PokerCard.NewPokerCards('2S', '3D', '4H', '5C', '6S'),
-      expected: true,
-    },
-    {
-      name: '2,3,4,5,6 of Hearts should be straight',
-      input: PokerCard.NewPokerCards('2H', '3H', '4H', '5H', '6H'),
-      expected: true,
-    },
-    {
-      name: '3,4,5,6,8 should not be straight',
-      input: PokerCard.NewPokerCards('3S', '4D', '5H', '6C', '8S'),
-      expected: false,
-    },
-    {
-      name: '10,J,Q,K,A of mixed suits should be straight',
-      input: PokerCard.NewPokerCards('0S', 'JD', 'QH', 'KC', 'AS'),
-      expected: true,
-    },
-    {
-      name: '10,J,Q,K,A of Spades should be straight',
-      input: PokerCard.NewPokerCards('0S', 'JS', 'QS', 'KS', 'AS'),
-      expected: true,
-    },
-    {
-      name: '10,6,8,9,7 not in order should be straight',
-      input: PokerCard.NewPokerCards('0S', '6D', '8H', '9C', '7S'),
-      expected: true,
-    },
-    {
-      name: '4,7,6,8,5,1 (6 cards with duplication) should be straight',
-      input: PokerCard.NewPokerCards('4S', '7D', '6H', '8C', '5S', 'AS'),
-      expected: true,
-    },
-    {
-      name: 'Q,8,9,8,8,10,J (7 cards with duplication) should be straight',
-      input: PokerCard.NewPokerCards('QS', '8D', '9H', '8C', '8S', '0H', 'JS'),
-      expected: true,
-    },
-    {
-      name: 'Q,8,10,8,8,10,J (7 cards with duplication) should not be straight',
-      input: PokerCard.NewPokerCards('QS', '8D', '0H', '8C', '8S', '0D', 'JS'),
-      expected: false,
-    },
-    {
-      name: '2,3,4,5,6,7,8 should be a straight as it contains a sequence of 5 cards',
-      input: PokerCard.NewPokerCards('2S', '3D', '4H', '5C', '6S', '7D', '8H'),
-      expected: true,
-    },
-  ];
+describe('Straight class', () => {
+  describe('Straight.isHand', () => {
+    const testCases = [
+      {
+        name: '4枚以下の場合はストレートではない',
+        input: PokerCard.NewPokerCards('AS', '2D', '3H', '4C'),
+        expected: false,
+      },
+      {
+        name: 'A,2,3,4,5 should be straight',
+        input: PokerCard.NewPokerCards('AS', '2D', '3H', '4C', '5S'),
+        expected: true,
+      },
+      {
+        name: '2,3,4,5,6 should be straight',
+        input: PokerCard.NewPokerCards('2S', '3D', '4H', '5C', '6S'),
+        expected: true,
+      },
+      {
+        name: '2,3,4,5,6 of Hearts should be straight',
+        input: PokerCard.NewPokerCards('2H', '3H', '4H', '5H', '6H'),
+        expected: true,
+      },
+      {
+        name: '3,4,5,6,8 should not be straight',
+        input: PokerCard.NewPokerCards('3S', '4D', '5H', '6C', '8S'),
+        expected: false,
+      },
+      {
+        name: '10,J,Q,K,A of mixed suits should be straight',
+        input: PokerCard.NewPokerCards('0S', 'JD', 'QH', 'KC', 'AS'),
+        expected: true,
+      },
+      {
+        name: '10,J,Q,K,A of Spades should be straight',
+        input: PokerCard.NewPokerCards('0S', 'JS', 'QS', 'KS', 'AS'),
+        expected: true,
+      },
+      {
+        name: '10,6,8,9,7 not in order should be straight',
+        input: PokerCard.NewPokerCards('0S', '6D', '8H', '9C', '7S'),
+        expected: true,
+      },
+      {
+        name: '4,7,6,8,5,1 (6 cards with duplication) should be straight',
+        input: PokerCard.NewPokerCards('4S', '7D', '6H', '8C', '5S', 'AS'),
+        expected: true,
+      },
+      {
+        name: 'Q,8,9,8,8,10,J (7 cards with duplication) should be straight',
+        input: PokerCard.NewPokerCards('QS', '8D', '9H', '8C', '8S', '0H', 'JS'),
+        expected: true,
+      },
+      {
+        name: 'Q,8,10,8,8,10,J (7 cards with duplication) should not be straight',
+        input: PokerCard.NewPokerCards('QS', '8D', '0H', '8C', '8S', '0D', 'JS'),
+        expected: false,
+      },
+      {
+        name: '2,3,4,5,6,7,8 should be a straight as it contains a sequence of 5 cards',
+        input: PokerCard.NewPokerCards('2S', '3D', '4H', '5C', '6S', '7D', '8H'),
+        expected: true,
+      },
+    ];
 
-  testCases.forEach(({ name, input, expected }) => {
-    test(name, () => {
-      expect(Straight.isHand(input)).toBe(expected);
+    testCases.forEach(({ name, input, expected }) => {
+      test(name, () => {
+        expect(Straight.isHand(input)).toBe(expected);
+      });
+    });
+  });
+
+  describe('isDraw', () => {
+    const testCases = [
+      {
+        name: '4 cards are given',
+        input: PokerCard.NewPokerCards('AH', '2D', '4S', '5C'),
+        expected: false,
+      },
+      {
+        name: 'valid 5 cards are given',
+        input: PokerCard.NewPokerCards('AH', '2D', '3S', '4C', '5H'),
+        expected: true,
+      },
+      {
+        name: 'valid 6 cards are given',
+        input: PokerCard.NewPokerCards('AH', '2D', '3S', '4C', '5H', '6S'),
+        expected: true,
+      },
+      {
+        name: '7 cards are given',
+        input: PokerCard.NewPokerCards('AH', '2D', '3S', '4C', '5H', '6S', '7D'),
+        expected: false,
+      },
+    ];
+
+    testCases.forEach(({ name, input, expected }) => {
+      it(name, () => {
+        const result = Straight.isDraw(input);
+        expect(result).toBe(expected);
+      });
+    });
+  });
+
+  describe('Straight.isMissingConsecutive ', () => {
+    const testCases = [
+      {
+        name: '4 cards are given, but one is missing for consecutive',
+        input: PokerCard.NewPokerCards('AS', '2D', '4S', '5H'),
+        expected: false,
+      },
+      {
+        name: 'valid 5 cards are given, but one is missing for consecutive',
+        input: PokerCard.NewPokerCards('2S', '3D', '5S', '6H', '0C'),
+        expected: true,
+      },
+      {
+        name: '5 cards are given, but two are missing for consecutive',
+        input: PokerCard.NewPokerCards('2S', '3D', '6S', '7H', '8C'),
+        expected: false,
+      },
+      {
+        name: 'valid 6 cards are given, but one is missing for consecutive',
+        input: PokerCard.NewPokerCards('2S', '3D', '5S', '6H', '9C', '0D'),
+        expected: true,
+      },
+      {
+        name: '6 cards are given, but two are missing for consecutive',
+        input: PokerCard.NewPokerCards('2S', '3D', '6S', '7H', '8C', '9D'),
+        expected: false,
+      },
+      {
+        name: '7 cards are given, but one is missing for consecutive',
+        input: PokerCard.NewPokerCards('2S', '3D', '5S', '6H', '7C', '8D', '9H'),
+        expected: false,
+      },
+    ];
+
+    testCases.forEach(({ name, input, expected }) => {
+      it(name, () => {
+        const result = Straight.isMissingConsecutive(input);
+        expect(result).toBe(expected);
+      });
+    });
+  });
+
+  describe('isAceToFiveStraight method', () => {
+    const testCases = [
+      {
+        name: '5 cards from Ace to 5',
+        input: PokerCard.NewPokerCards('AS', '2D', '3H', '4C', '5S'),
+        expected: true,
+      },
+      {
+        name: '6 cards from Ace to 6',
+        input: PokerCard.NewPokerCards('AS', '2D', '3H', '4C', '5S', '6H'),
+        expected: true,
+      },
+      {
+        name: '5 cards from 2 to 6',
+        input: PokerCard.NewPokerCards('2D', '3H', '4C', '5S', '6H'),
+        expected: false,
+      },
+    ];
+
+    testCases.forEach(({ name, input, expected }) => {
+      test(name, () => {
+        const result = Straight.isAceToFiveStraight(input);
+        expect(result).toBe(expected);
+      });
+    });
+  });
+
+  describe('isConsecutive method', () => {
+    const cases = [
+      {
+        name: '4 cards, consecutive numbers',
+        input: PokerCard.NewPokerCards('4D', '5S', '6H', '7C'),
+        startIndex: 0,
+        length: 4,
+        expected: true,
+      },
+      {
+        name: '4 cards, non-consecutive numbers',
+        input: PokerCard.NewPokerCards('4D', '6S', '7H', '8C'),
+        startIndex: 0,
+        length: 4,
+        expected: false,
+      },
+      {
+        name: '5 cards, consecutive numbers at start',
+        input: PokerCard.NewPokerCards('2D', '3S', '4H', '5C', '7S'),
+        startIndex: 0,
+        length: 4,
+        expected: true,
+      },
+      {
+        name: '5 cards, consecutive numbers at end',
+        input: PokerCard.NewPokerCards('3D', '5S', '6H', '7C', '8S'),
+        startIndex: 1,
+        length: 4,
+        expected: true,
+      },
+      {
+        name: '5 cards, all non-consecutive',
+        input: PokerCard.NewPokerCards('2D', '4S', '6H', '8C', '0S'),
+        startIndex: 0,
+        length: 4,
+        expected: false,
+      },
+      {
+        name: '6 cards, consecutive numbers in the middle',
+        input: PokerCard.NewPokerCards('2D', '4S', '5H', '6C', '7S', '9H'),
+        startIndex: 1,
+        length: 4,
+        expected: true,
+      },
+      {
+        name: '7 cards, consecutive numbers at start',
+        input: PokerCard.NewPokerCards('2D', '3S', '4H', '5C', '7S', '9H', '0C'),
+        startIndex: 0,
+        length: 4,
+        expected: true,
+      },
+      {
+        name: '7 cards, all non-consecutive',
+        input: PokerCard.NewPokerCards('2D', '4S', '6H', '8C', '0S', '0D', 'QC'),
+        startIndex: 0,
+        length: 4,
+        expected: false,
+      },
+    ];
+
+    cases.forEach((test) => {
+      it(test.name, () => {
+        const result = Straight.isConsecutive(test.input, test.startIndex, test.length);
+        expect(result).toBe(test.expected);
+      });
     });
   });
 });
--- src/lib/domain/model/hands/straight.ts
+++ src/lib/domain/model/hands/straight.ts
@@ -5,6 +5,10 @@ import { PokerHand, PokerHandRank } from '@/lib/domain/model/hands/hands';
 export class Straight extends PokerHand {
   static readonly score: PokerHandRank = PokerHandRank.STRAIGHT;
 
+  /**
+   * 5枚以上のカードが引数として渡された場合、ストレートかどうかを判定する
+   * @param cards
+   */
   static isHand(cards: PokerCard[]): boolean {
     // 4枚以下の場合はストレートではない
     if (cards.length <= 4) {
@@ -37,8 +41,89 @@ export class Straight extends PokerHand {
     return consecutiveCount >= 5;
   }
 
-  private static isAceToFiveStraight(cards: PokerCard[]): boolean {
+  /**
+   * 5枚以上のカードが引数として渡された場合、ストレートドローかどうかを判定する
+   * @param cards
+   */
+  public static isDraw(cards: PokerCard[]): boolean {
+    // ゲーム上めくれている枚数が4枚以下のケースは考慮不要
+    // 6枚超過の場合はストレートドローではない(7枚の時点で、もうカード追加の可能性はないため)
+    if (cards.length <= 4 || cards.length > 6) {
+      return false;
+    }
+
+    const sortedCards = CardsSorter.byNumber(cards);
+
+    // 4連続しているカードの確認
+    for (let i = 0; i < sortedCards.length - 3; i++) {
+      if (this.isConsecutive(sortedCards, i, 4)) {
+        return true;
+      }
+    }
+
+    // 中が抜けているカードの確認
+    if (this.isMissingConsecutive(sortedCards)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  /**
+   * カードのシーケンスにで一つだけ欠けがあるどうかを判定する
+   * @param sortedCards
+   */
+  public static isMissingConsecutive(sortedCards: PokerCard[]): boolean {
+    // 7枚以上ある時点でもう可能性はない
+    if (sortedCards.length >= 7) {
+      return false;
+    }
+
+    for (let i = 0; i <= sortedCards.length - 4; i++) {
+      const diff1 = sortedCards[i + 1].cardNumber - sortedCards[i].cardNumber;
+      const diff2 = sortedCards[i + 2].cardNumber - sortedCards[i + 1].cardNumber;
+      const diff3 = sortedCards[i + 3].cardNumber - sortedCards[i + 2].cardNumber;
+
+      if (
+        (diff1 === 1 && diff2 === 2 && diff3 === 1) || // 欠けている数値が始まりの場合
+        (diff1 === 1 && diff2 === 1 && diff3 === 2) || // 欠けている数値が終わりの場合
+        (diff1 === 2 && diff2 === 1 && diff3 === 1) // 欠けている数値が真ん中の場合
+      ) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  /*
+   * エースを含むストレートかどうかを判定する
+   */
+  public static isAceToFiveStraight(cards: PokerCard[]): boolean {
+    if (cards.length < 5) {
+      return false;
+    }
     const aceToFives = [14, 2, 3, 4, 5];
-    return cards.every((card) => aceToFives.includes(card.cardNumber));
+    const aceToFiveCount = cards.filter((card) => aceToFives.includes(card.cardNumber));
+    return aceToFiveCount.length >= 5;
+  }
+
+  /**
+   * カードのシーケンスが連続しているかどうかを判定する
+   * @param cards
+   * @param startIndex
+   * @param length
+   */
+  public static isConsecutive(
+    cards: PokerCard[],
+    startIndex: number,
+    length: number,
+  ): boolean {
+    for (let i = 0; i < length - 1; i++) {
+      if (cards[startIndex + i].cardNumber + 1 !== cards[startIndex + i + 1].cardNumber) {
+        return false;
+      }
+    }
+    return true;
   }
 }
