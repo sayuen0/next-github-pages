ecbf3642a45001b5d8f1c97ca00c4b85fdf9dba8
sayuen0 <sayuen0@gmail.com>
2023-10-01 17:41:40 +0900

    add holeCard

---
+++ src/lib/domain/model/holeCards.test.ts
@@ -0,0 +1,54 @@
+import { HoleCards } from '@/lib/domain/model/holeCards';
+import { CardValue, PokerCard } from '@/lib/domain/model/card';
+
+describe('HoleCards', () => {
+  describe('isPocketPair', () => {
+    type TestCase = {
+      input1: CardValue; // テストのための1枚目のカード
+      input2: CardValue; // テストのための2枚目のカード
+      expected: boolean; // 期待する結果
+    };
+
+    const testCases: TestCase[] = [
+      { input1: 'AH', input2: 'AS', expected: true },
+      { input1: '5H', input2: '5D', expected: true },
+      { input1: 'KH', input2: 'KD', expected: true },
+      { input1: 'AH', input2: 'KH', expected: false },
+      { input1: '5H', input2: '6D', expected: false },
+    ];
+
+    testCases.forEach(({ input1, input2, expected }) => {
+      test(`returns ${expected} when cards are ${input1} and ${input2}`, () => {
+        const card1 = new PokerCard(input1);
+        const card2 = new PokerCard(input2);
+        const holeCards = new HoleCards(card1, card2);
+        expect(holeCards.isPocketPair()).toBe(expected);
+      });
+    });
+  });
+
+  describe('isSuited', () => {
+    type TestCase = {
+      input1: CardValue; // テストのための1枚目のカード
+      input2: CardValue; // テストのための2枚目のカード
+      expected: boolean; // 期待する結果
+    };
+
+    const testCases: TestCase[] = [
+      { input1: 'AH', input2: 'KH', expected: true },
+      { input1: '5H', input2: '7H', expected: true },
+      { input1: 'QH', input2: 'QH', expected: true }, // 同じカードでもスートが一致するのでtrue
+      { input1: 'AD', input2: 'KH', expected: false },
+      { input1: '5H', input2: '5D', expected: false },
+    ];
+
+    testCases.forEach(({ input1, input2, expected }) => {
+      test(`returns ${expected} when cards are ${input1} and ${input2}`, () => {
+        const card1 = new PokerCard(input1);
+        const card2 = new PokerCard(input2);
+        const holeCards = new HoleCards(card1, card2);
+        expect(holeCards.isSuited()).toBe(expected);
+      });
+    });
+  });
+});
---
+++ src/lib/domain/model/holeCards.ts
@@ -0,0 +1,23 @@
+import { PokerCard } from '@/lib/domain/model/card';
+
+export class HoleCards {
+  private card1: PokerCard;
+  private card2: PokerCard;
+
+  constructor(card1: PokerCard, card2: PokerCard) {
+    this.card1 = card1;
+    this.card2 = card2;
+  }
+
+  get cards(): [PokerCard, PokerCard] {
+    return [this.card1, this.card2];
+  }
+
+  isPocketPair(): boolean {
+    return this.card1.cardNumber === this.card2.cardNumber;
+  }
+
+  isSuited(): boolean {
+    return this.card1.suit === this.card2.suit;
+  }
+}
