a407dd7fe2374fa32fa4480da8a64304ee0f6e72
sayuen0 <sayuen0@gmail.com>
2023-10-01 16:06:29 +0900

    add chip image

--- src/pages/demo/singleChip.tsx
+++ src/pages/demo/singleChip.tsx
@@ -1,6 +1,20 @@
+import { useState } from 'react';
+
 export default function SingleChip() {
+  const [size, setSize] = useState({ width: 100, height: 100 }); // 初期サイズを設定
+
   // TODO: コインの画像を取得する
   // TODO: ドロップダウンを用意し、その額のコインの計算をするようにする
   // TODO: 100円単位での自由入力を設け、その額のコインを最大桁数で表示するようにする
-  return <div>{/* コンポーネントの内容 */}</div>;
+  return (
+    <div>
+      {
+        <img
+          src="/static/img/chips/500.svg"
+          style={{ width: `${size.width}px`, height: `${size.height}px` }}
+          alt="500.svg"
+        />
+      }
+    </div>
+  );
 }
