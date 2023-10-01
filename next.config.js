fe5b5ef94650a9e8de8cdfbcfbdfd8455c863427
sayuen0 <sayuen0@gmail.com>
2023-10-01 14:28:43 +0900

    CIでベースパスを設定

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  exportPathMap: function(){
-      return {
-"/": {page: "/"}
-      }
+const isProduction = process.env.NODE_ENV === "production";
 
-  }
+const nextConfig = {
+    basePath: isProduction ? process.env.BASE_PATH : "",
+    assetPrefix: isProduction ? process.env.ASSET_PREFIX : "",
+
+    exportPathMap: function () {
+        return {
+            "/": {page: "/"}
+        }
+
+    }
 }
 
 module.exports = nextConfig
