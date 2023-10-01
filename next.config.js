6463857a16049d8b8ff335b8dc1119a51ad31577
sayuen0 <sayuen0@gmail.com>
2023-10-01 14:56:15 +0900

    再帰的routing

--- next.config.js
+++ next.config.js
@@ -1,32 +1,54 @@
-const { promises: fsPromises } = require('fs');
 const path = require('path');
+const fs = require('fs').promises;
 
 /** @type {import("next").NextConfig} */
 const isProduction = process.env.NODE_ENV === 'production';
+const pagesDirectory = path.join(__dirname, 'src/pages');
 
-module.exports = {
+async function getFilesInDirectory(dir) {
+  const files = await fs.readdir(dir);
+  const fileAndDirectoryPaths = await Promise.all(
+    files.map(async (file) => {
+      const fullPath = path.join(dir, file);
+      const isDirectory = (await fs.stat(fullPath)).isDirectory();
+      if (isDirectory) {
+        return getFilesInDirectory(fullPath);
+      } else {
+        return [fullPath.replace(/\.tsx$/, '').replace(pagesDirectory, '')];
+      }
+    }),
+  );
+  return fileAndDirectoryPaths.flat();
+}
+
+const nextConfig = {
   basePath: isProduction ? process.env.BASE_PATH : '',
   assetPrefix: isProduction ? process.env.ASSET_PREFIX : '',
 
   async exportPathMap() {
-    const filePaths = await fsPromises.readdir(path.join(__dirname, './pages'));
-    const pagePaths = filePaths.map(
-      (filePath) => `/${filePath.replace('.js', '')}`,
-    );
+    const files = await getFilesInDirectory(pagesDirectory);
 
-    // Convert file paths to export path map
-    return pagePaths.reduce((acc, curr) => {
-      // Skip certain filenames or directories as required
-      if (curr.startsWith('/_') || curr.match(/[A-Z]/)) {
-        return acc;
+    return files.reduce((acc, filePath) => {
+      const route = filePath.replace(/\/index$/, ''); // 把 /index 替换为 /
+
+      // Avoid special Next.js files and API routes
+      if (
+        !route.includes('/[') &&
+        !route.includes('_') &&
+        !route.includes('/api')
+      ) {
+        if (!route.startsWith('/')) {
+          console.error(`Invalid page path: ${route}`);
+        } else {
+          acc[route] = {
+            page: filePath,
+          };
+        }
       }
 
-      return {
-        ...acc,
-        [curr]: {
-          page: curr,
-        },
-      };
+      return acc;
     }, {});
   },
 };
+
+module.exports = nextConfig;
