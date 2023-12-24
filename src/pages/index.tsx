32ff6438d8fe374a97f66e0b1f82eb257ac327f4
sayuen0 <sayuen0@gmail.com>
2023-12-24 18:19:43 +0900

    fix entrypoint page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import { Inter } from 'next/font/google';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function HomePage() {
-  /**
-   * "/texasHoldem"へのリンクを作成する
-   */
   return (
     <div>
-      <a href="/ultimate">Ultimate</a>
+      <Link href="/ultimate">アルティメット</Link>
     </div>
   );
 }
