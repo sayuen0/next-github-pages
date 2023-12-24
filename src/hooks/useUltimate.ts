47bafd7419817108c8db256efd37fb4aee8b5f32
sayuen0 <sayuen0@gmail.com>
2023-12-24 18:16:48 +0900

    hooksディレクトリに引越し

---
+++ src/hooks/useUltimate.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import { UltimateTexasHoldem } from '@/lib/domain/model/game/texasHoldem/ultimate/ultimate';
+import { Player } from '@/lib/domain/model/players/player';
+import { PokerCard } from '@/lib/domain/model/cards/card';
+import { RandomPokerCardGenerator } from '@/lib/domain/model/cards/randomCardGenerator';
+
+export const useUltimate = () => {
+  const [game, setGame] = useState<UltimateTexasHoldem | null>(null);
+  const [player, setPlayer] = useState<Player | null>(null);
+  const [dealerCards, setDealerCards] = useState<PokerCard[]>([]);
+  const [playerCards, setPlayerCards] = useState<PokerCard[]>([]);
+  const [communityCards, setCommunityCards] = useState<PokerCard[]>([]);
+
+  function generateRandomCards(count: number): PokerCard[] {
+    return Array.from({ length: count }, () =>
+      RandomPokerCardGenerator.getRandomCard(true),
+    );
+  }
+
+  useEffect(() => {
+    // ゲームとプレイヤーの初期化
+    const newPlayer = new Player('Player1', 30000);
+    const newGame = new UltimateTexasHoldem(new Player('Dealer', 1000000), newPlayer);
+    setGame(newGame);
+    setPlayer(newPlayer);
+
+    setDealerCards(generateRandomCards(2));
+    setPlayerCards(generateRandomCards(2));
+    setCommunityCards(generateRandomCards(5));
+    // その他のゲームセットアップ処理
+  }, []);
+
+  return { game, player, dealerCards, playerCards, communityCards };
+};
--- src/pages/ultimate.tsx
+++ src/pages/ultimate.tsx
@@ -1,38 +1,10 @@
 // Ultimate コンポーネント
 import Card from '@/components/cards/card';
-import { RandomPokerCardGenerator } from '@/lib/domain/model/cards/randomCardGenerator';
-import { useEffect, useState } from 'react';
-import { PokerCard } from '@/lib/domain/model/cards/card';
 import Table from '@/components/table/table';
-import { Player } from '@/lib/domain/model/players/player';
-import { UltimateTexasHoldem } from '@/lib/domain/model/game/texasHoldem/ultimate/ultimate';
+import { useUltimate } from '@/hooks/useUltimate';
 
 export default function Ultimate() {
-  const [game, setGame] = useState<UltimateTexasHoldem | null>(null);
-  const [player, setPlayer] = useState<Player | null>(null);
-  const [dealerCards, setDealerCards] = useState<PokerCard[]>([]);
-  const [playerCards, setPlayerCards] = useState<PokerCard[]>([]);
-  const [communityCards, setCommunityCards] = useState<PokerCard[]>([]);
-
-  function generateRandomCards(count: number): PokerCard[] {
-    return Array.from({ length: count }, () =>
-      RandomPokerCardGenerator.getRandomCard(true),
-    );
-  }
-
-  useEffect(() => {
-    // ゲームとプレイヤーの初期化
-    const newPlayer = new Player('Player1', 30000);
-    const newGame = new UltimateTexasHoldem(new Player('Dealer', 1000000), newPlayer);
-    setGame(newGame);
-    setPlayer(newPlayer);
-
-    setDealerCards(generateRandomCards(2));
-    setPlayerCards(generateRandomCards(2));
-    setCommunityCards(generateRandomCards(5));
-    // その他のゲームセットアップ処理
-  }, []);
-
+  const { game, player, dealerCards, playerCards, communityCards } = useUltimate();
   const cardsContainerStyle = {
     display: 'flex', // フレックスボックスを適用
     justifyContent: 'center', // 中央揃え
