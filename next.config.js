f04cf32ef4dcde193d7459026f51dc2a4a21b58f
sayuen0 <sayuen0@gmail.com>
2024-03-07 14:08:40 +0900

    PWA化

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 const fs = require('fs').promises;
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
 
-/** @type {import("next").NextConfig} */
-
-const nextConfig = {
-  output: 'export',
-};
-
-module.exports = nextConfig;
+module.exports = withPWA({
+  //next.js config
+  reactStrictMode: true,
+});
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ja">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png"></link>
+        <meta name="theme-color" content="#fff" />
+      </Head>
       <body>
         <Main />
         <NextScript />
